5547df9a146457ad23d136ab621b27abdb7304c9
Mauricio Fernandes de Castro <mauricio@iservport.com>
2015-11-09 14:29:51 -0200

    Refactored senders.

--- src/main/java/org/helianto/sender/service/PasswordRecoverySender.java
+++ src/main/java/org/helianto/sender/service/PasswordRecoverySender.java
@@ -1,11 +1,7 @@
 package org.helianto.sender.service;
 
-import java.util.Map;
-
 import javax.inject.Inject;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
